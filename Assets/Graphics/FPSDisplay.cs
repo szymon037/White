7675357e19118b9fb95fb3e5e2e742a26bc5ed01
Szymon Omieciński <szymon.lost@gmail.com>
2022-01-25 11:31:23 +0100

    Project version + unlit materials

---
+++ Assets/Graphics/FPSDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public int avgFrameRate;
+    public Text display_Text;
+    bool updateCheck = true;
+
+    public void Update()
+    {
+        updateCheck = !updateCheck;
+        if (!updateCheck)
+            return;
+
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = "FPS: " + avgFrameRate.ToString();
+    }
+}
